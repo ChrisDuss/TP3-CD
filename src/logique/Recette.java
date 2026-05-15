f29796ad9d18d0a2bb94f31834802898e1ecffc0
Christophe Dussault <136032697+ChrisDuss@users.noreply.github.com>
2026-05-15 13:00:05 -0400

    Ajout tests Recette

--- src/logique/Recette.java
+++ src/logique/Recette.java
@@ -3,26 +3,35 @@ package logique;
 import java.util.ArrayList;
 
 /**
- * Author : Mathieu Bourgoin
- * Ordre de conception : 3e
+ * Author : Christophe Dussault
+ * Ordre de conception : 2e
  */
-public class Recette
-{
+public class Recette {
+    public static final String NOM_TROP_COURT = "nom trop court";
+    public static final String NOM_NULL = "nom ne peut pas être null";
+    public static final String AUCUN_INGREDIENT_NULL = "Aucun ingrédient ne peut être null";
+    public static final String INGREDIENTS_UNIQUE = "Les ingrédients doivent tout être unique";
+    public static final String NOT_IN_RANGE = "difficulté not in range";
+    public static final String EXPERIENCE_SUPERIEUR_ZERO = "expérience est strictement supérieur a zéro";
+    public static final int LONGEUR_NOM_MIN = 10;
     private ArrayList<Ingredient> ingredients;
     private String nom;
     private int difficulte;
     private int pointExperience;
 
-    public Recette(Ingredient ing1, Ingredient ing2, Ingredient ing3, String nom, int difficulte, int pointExperience)
-    {
-        this.ingredients = new ArrayList<Ingredient>();
-        this.ingredients.add(ing1);
-        this.ingredients.add(ing2);
-        this.ingredients.add(ing3);
+    public Recette(Ingredient ing1, Ingredient ing2, Ingredient ing3, String nom, int difficulte, int pointExperience) {
+        if (ing1 == null || ing2 == null || ing3 == null) throw new IllegalArgumentException(AUCUN_INGREDIENT_NULL);
+        else if (ing1 == ing2 || ing2 == ing3 || ing3 == ing1) throw new IllegalArgumentException(INGREDIENTS_UNIQUE);
+        else {
+            this.ingredients = new ArrayList<Ingredient>();
+            this.ingredients.add(ing1);
+            this.ingredients.add(ing2);
+            this.ingredients.add(ing3);
 
-        this.setDifficulte(difficulte);
-        this.setNom(nom);
-        this.setPointExperience(pointExperience);
+            this.setDifficulte(difficulte);
+            this.setNom(nom);
+            this.setPointExperience(pointExperience);
+        }
     }
 
 
@@ -31,7 +40,9 @@ public class Recette
     }
 
     private void setNom(String nom) {
-        this.nom = nom;
+        if (nom == null) throw new IllegalArgumentException(NOM_NULL);
+        else if (nom.length() < LONGEUR_NOM_MIN) throw new IllegalArgumentException(NOM_TROP_COURT);
+        else this.nom = nom;
     }
 
     public int getDifficulte() {
@@ -39,7 +50,8 @@ public class Recette
     }
 
     private void setDifficulte(int difficulte) {
-        this.difficulte = difficulte;
+        if (difficulte < 1 || difficulte > 5) throw new IllegalArgumentException(NOT_IN_RANGE);
+        else this.difficulte = difficulte;
     }
 
     public int getPointExperience() {
@@ -47,11 +59,11 @@ public class Recette
     }
 
     private void setPointExperience(int pointExperience) {
-        this.pointExperience = pointExperience;
+        if (pointExperience <= 0) throw new IllegalArgumentException(EXPERIENCE_SUPERIEUR_ZERO);
+        else this.pointExperience = pointExperience;
     }
 
-    public int obtenirPrix()
-    {
+    public int obtenirPrix() {
         int prixTotal = 0;
 
         for (Ingredient ing : this.ingredients)
@@ -60,24 +72,25 @@ public class Recette
         return prixTotal;
     }
 
-    public boolean contientIngredient(String nom)
-    {
-        boolean estContenu = false;
+    public boolean contientIngredient(String nom) {
+        if (nom == null) throw new IllegalArgumentException(NOM_NULL);
+        else {
+            boolean estContenu = false;
 
-        for (Ingredient ing : this.ingredients)
-        {
-            if (ing.getNom().equals(nom))
-            {
-                estContenu = true;
-                break;
+
+            for (Ingredient ing : this.ingredients) {
+                if (ing.getNom().equals(nom)) {
+                    estContenu = true;
+                    break;
+                }
             }
-        }
 
-        return estContenu;
+            return estContenu;
+        }
     }
+
     @Override
-    public String toString()
-    {
+    public String toString() {
         return String.format("%s|%s|%s|%s|%s|%s", this.getNom(), this.ingredients.get(0).getNom(), this.ingredients.get(1).getNom(), this.ingredients.get(2).getNom(), this.getDifficulte(), this.getPointExperience());
     }
 
--- src/tests/TestsRecette.java
+++ src/tests/TestsRecette.java
@@ -1,5 +1,10 @@
 package tests;
 
+import logique.Ingredient;
+import logique.Recette;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * TP3 - CD.iml
  *
@@ -7,4 +12,107 @@ package tests;
  * @since 5/13/2026
  */
 public class TestsRecette {
+    Ingredient ing1 = new Ingredient("Carrot", 2);
+    Ingredient ing2 = new Ingredient("Patate", 5);
+    Ingredient ing3 = new Ingredient("Amethyst", 20);
+    String nomInit = "Recette_soupe";
+    int diffInit = 2;
+    int expInint = 3;
+    Recette rec = null;
+
+    @BeforeEach
+    public void Recette_Initialisation() {
+        rec = new Recette(ing1, ing2, ing3, nomInit, diffInit, expInint);
+    }
+
+    @Test
+    public void Recette_Constructeur_Aucun_Ingredient_null() {
+        Exception ex = assertThrows(IllegalArgumentException.class, ()->
+                rec = new Recette(ing1, null, ing3, nomInit, diffInit, expInint));
+
+        assertEquals(Recette.AUCUN_INGREDIENT_NULL, ex.getMessage());
+    }
+
+    @Test
+    public void Recette_Constructeur_Ingredient_Unique() {
+        Exception ex = assertThrows(IllegalArgumentException.class, ()->
+                rec = new Recette(ing1, ing1, ing3, nomInit, diffInit, expInint));
+
+        assertEquals(Recette.INGREDIENTS_UNIQUE, ex.getMessage());
+    }
+
+    @Test
+    public void Recette_Constructeur_setNom_Non_Null() {
+        Exception ex = assertThrows(IllegalArgumentException.class, ()->
+                rec = new Recette(ing1, ing2, ing3, null, diffInit, expInint));
+
+        assertEquals(Recette.NOM_NULL, ex.getMessage());
+    }
+
+    @Test
+    public void Recette_Constructeur_setNom_Nom_Trop_Court() {
+        Exception ex = assertThrows(IllegalArgumentException.class, ()->
+                rec = new Recette(ing1, ing2, ing3, "nomInit", diffInit, expInint));
+
+        assertEquals(Recette.NOM_TROP_COURT, ex.getMessage());
+    }
+
+    @Test
+    public void Recette_Constructeur_setNom() {
+        assertEquals(rec.getNom(), nomInit);
+    }
+
+    @Test
+    public void Recette_Constructeur_setDifficulte_Plus_bas_1() {
+        Exception ex = assertThrows(IllegalArgumentException.class, ()->
+                rec = new Recette(ing1, ing2, ing3, nomInit, 0, expInint));
+
+        assertEquals(Recette.NOT_IN_RANGE, ex.getMessage());
+    }
+
+    @Test
+    public void Recette_Constructeur_setDifficulte_Plus_Haut_5() {
+        Exception ex = assertThrows(IllegalArgumentException.class, ()->
+                rec = new Recette(ing1, ing2, ing3, nomInit, 6, expInint));
+
+        assertEquals(Recette.NOT_IN_RANGE, ex.getMessage());
+    }
+
+    @Test
+    public void Recette_Constructeur_setDifficulte() {
+        assertEquals(rec.getDifficulte(), diffInit);
+    }
+
+    @Test
+    public void Recette_Constructeur_setPointExperiance_Superieur_a_zero() {
+        Exception ex = assertThrows(IllegalArgumentException.class, ()->
+                rec = new Recette(ing1, ing2, ing3, nomInit, diffInit, 0));
+
+        assertEquals(Recette.EXPERIENCE_SUPERIEUR_ZERO, ex.getMessage());
+    }
+
+    @Test
+    public void Recette_Constructeur_setPointExperiance() {
+        assertEquals(rec.getPointExperience(), expInint);
+    }
+
+    @Test
+    public void Recette_obtenirPrix() {
+        int total = ing1.getPrix() + ing2.getPrix() + ing3.getPrix();
+        assertEquals(total, rec.obtenirPrix());
+    }
+
+    @Test
+    public void Recette_contientIngredient_non_null() {
+        Exception ex = assertThrows(IllegalArgumentException.class, ()->
+                rec.contientIngredient(null));
+
+        assertEquals(Recette.NOM_NULL, ex.getMessage());
+    }
+
+    @Test
+    public void Recette_contientIngredient() {
+        assertTrue(rec.contientIngredient(ing1.getNom()));
+    }
 }
+
